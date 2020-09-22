152384daec30cf4791a959ab3feb3662fa54d000
滨 <652494798@qq.com>
2020-09-22 22:41:44 +0800

    优化文章处理

--- 3118005421/src/main/java/util/articleUtil.java
+++ 3118005421/src/main/java/util/articleUtil.java
@@ -1,11 +1,13 @@
 package util;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class articleUtil {
 
 
-    public String[] divisionArticle(File article) throws IOException {
+    public List<String> divisionArticle(File article) throws IOException {
         BufferedReader bufferedReader=new BufferedReader(new FileReader(article));
         String temp;
         StringBuilder stringBuilder=new StringBuilder();
@@ -14,11 +16,12 @@ public class articleUtil {
         }
         String[] sentences=stringBuilder.toString()
                 .replace(" ", "")
-                .replace("。","，")
-                .replace("：“","")
                 .replace("”","")
-                .replace("？","，")
-                .split("，");
-        return sentences;
+                .replace("：“","")
+                .replace("？","。")
+                .split("。");
+        List<String> list = Arrays.asList(sentences);
+        return list;
     }
+
 }
