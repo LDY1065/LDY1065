dbf94282264dab0e50fd91c1183b4ad92f5ef5d0
滨 <652494798@qq.com>
2020-09-24 16:30:52 +0800

    添加了一些异常处理

--- 3118005421/src/main/java/application/application.java
+++ 3118005421/src/main/java/application/application.java
@@ -4,9 +4,7 @@ import service.calculate;
 import util.articleUtil;
 import util.fileUtil;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -16,7 +14,7 @@ import java.util.concurrent.RecursiveTask;
 
 public class application {
 
-    private static final Integer MAX = 45;
+    private static final Integer MAX = 5;
     private static articleUtil articleUtil=new articleUtil();
     private static calculate calculate=new calculate();
 
@@ -32,7 +30,7 @@ public class application {
 
         @Override
         protected Double compute() {
-            if(copyDivision.size()>MAX){
+            if(copyDivision.size()>=MAX){
                 myForkJoinTask subTask1=new myForkJoinTask(originalDivision,copyDivision.subList(0,copyDivision.size()/2));
                 myForkJoinTask subTask2=new myForkJoinTask(originalDivision,copyDivision.subList(copyDivision.size()/2,copyDivision.size()));
                 invokeAll(subTask1,subTask2);
@@ -58,22 +56,37 @@ public class application {
         }
 
     }
+
     public static void main(String[] args) throws IOException {
-        File original=new File("E:/test/orig.txt");
-        File copy=new File("E:/test/orig_0.8_add.txt");
+        if(args.length<3){
+            System.out.println("请输入正确的命令行参数");
+            System.exit(0);
+        }
+        File original=new File(args[0]);
+        File copy=new File(args[1]);
+        if(!original.isFile()||!copy.isFile()){
+            System.out.println("请输入正确的路径");
+            return;
+        }
+        if(original.length()==0||copy.length()==0){
+            System.out.println("请不要输入空文件");
+            return;
+        }
         List<String> originalDivision = articleUtil.divisionArticle(original);
         List<String> copyDivision = articleUtil.divisionArticle(copy);
         ForkJoinPool pool=new ForkJoinPool();
         ForkJoinTask<Double> taskFuture=pool.submit(new myForkJoinTask(originalDivision,copyDivision));
         try {
             Double result = taskFuture.get();
-            fileUtil.writeAnswerToFile(result.toString(),"E:/test/answer.txt");
+            if(!fileUtil.writeAnswerToFile(String.format(result.toString(),"%.2f"),args[2])){
+                System.out.println("输出文件路径有误");
+                return;
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
-
         /*double max=0;
         double sum=0;
         for (String s : copyDivision) {
